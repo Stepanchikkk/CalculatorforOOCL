0a7fd80a2866e437a7195fea0cad35f26d31a531
Stepanchikkk <146267684+Stepanchikkk@users.noreply.github.com>
2023-09-28 01:26:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function calc(L, l, a, wdl, conn, conl) {
+    var N = Math.ceil(L/l) - 1
+    var A = L * a + N * wdl + conn * conl
+    return A
+}
+
+function getResult() {
+    var L = document.getElementById("lineLength").value
+    var l = document.getElementById("constructLength").value
+    var a = document.getElementById("attenuationCoeff").value
+    var wdl = document.getElementById("weldedLosses").value
+    var conn = document.getElementById("connectionsNumber").value
+    var conl = document.getElementById("connectionLosses").value
+    
+    var A = calc(L, l, a, wdl, conn, conl)
+    console.log(A)
+
+    document.getElementById("dat").innerHTML = A
+}
