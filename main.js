3437cf044abde2edb88f0d4df502b7778f084bc7
Stepanchikkk <146267684+Stepanchikkk@users.noreply.github.com>
2023-11-26 21:37:26 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
+// Функция для рассчета потерь на линии
 function calc(L, l, a, wdl, conn, conl) {
+    // Рассчитываем количество сегментов линии
     var N = Math.ceil(L/l) - 1
-    var A = L * a + N * wdl + conn * conl
+    // Рассчитываем общие потери на линии
+    var A = L * parseFloat(a.replace(",", ".")) + N * parseFloat(wdl.replace(",", ".")) + parseFloat(conn.replace(",", ".")) * parseFloat(conl.replace(",", "."))
+    // Возвращаем общие потери
     return A
 }
 
+// Функция для получения результата
 function getResult() {
+    // Получаем значения из полей ввода
     var L = document.getElementById("lineLength").value
     var l = document.getElementById("constructLength").value
     var a = document.getElementById("attenuationCoeff").value
     var wdl = document.getElementById("weldedLosses").value
     var conn = document.getElementById("connectionsNumber").value
     var conl = document.getElementById("connectionLosses").value
-    
-    var A = calc(L, l, a, wdl, conn, conl)
-    console.log(A)
 
+    // Вызываем функцию для рассчета потерь на линии
+    var A = calc(L, l, a, wdl, conn, conl)
+    // Формируем текст с результатом
     var resText = "Потери на линии составят " + A + " дБ"
+    // Выводим результат на страницу
     document.getElementById("dat").innerHTML = resText
 }
